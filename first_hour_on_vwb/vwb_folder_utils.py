acd87f4f962ca27cbdf7d3df306f3a5f6ee9ccda
Emma Rogge <emmarogge@google.com>
2023-10-25 14:14:33 +0000

    Address reviewer feedback
    Remove characters delimiting data collection description so that they do not appear in UI. Refactor utilities file due to [implementation of JSON-formatted output of workspace folder tree via Workbench CLI](https://github.com/verily-src/terra-tool-cli/pull/319). Add pytest unit tests for utility methods.

--- first_hour_on_vwb/vwb_folder_utils.py
+++ first_hour_on_vwb/vwb_folder_utils.py
@@ -1,115 +1,27 @@
+import copy as mycopy
+import json
 import re
 import subprocess
 import sys
 import tempfile
   
+def traverse_tree(name: str, tree: []):
+    for item in tree:
+        current_name = item['name'].strip()
+        if current_name == name:
+            return item
+        if len(item['children']) != 0:
+            tree_child = traverse_tree(name, item['children'])
+            if tree_child is not None:
+                return tree_child
 
-# Constants for parsing output of CLI command `terra folder tree`.
-FIRST_PARENT_REGEX = r"^\├\─\─\s(.+)$"
-LAST_PARENT_REGEX = r"^\└\─\─(.+)$"
-LEAF_REGEX = r"^\│(\s+)\├\─\─(.+)$"
-LAST_LEAF_REGEX = r"^\│(\s+)\└\─\─(.+)$"
-DANGLING_LINE_REGEX = r"^(\s+)\└\─\─(.+)$"
-TOP_LEVEL_CLEAN_LINE_TO_YAML_REGEX = r"^(.*)\s\((.*)\)$|^[\s*](.*)\s\((.*)\)\s*$"
-CLEAN_LINE_TO_YAML_REGEX = r"^(\s+)(.*)\s\((.*)\)\s*$"
-
-def parse_tree(cli_output : str = None):
+def get_folder_id(name: str, tree: []):
     """
-    Returns a representation of the workspace folder tree in the form of a list of dictionaries.
-    """
-    # 0) Create temp files for reading/writing text representation of folder tree.
-    sys.stdout.flush()
-    cli_output_tree = tempfile.NamedTemporaryFile(mode = 'w+', delete=False)
-    parsed_tree = tempfile.NamedTemporaryFile(mode = 'w+', delete=False)
-
-    # 1) Get workspace folder tree, if not provided.
-    if cli_output is None:
-        cli_folder_tree_output = subprocess.run(
-            ["terra", "folder", "tree"], capture_output=True, text=True
-        )
-        cli_output = cli_folder_tree_output.stdout
-
-    # 2) Write workspace folder tree to temp file.
-    with open(cli_output_tree.name, "w+") as cli_tree:
-        cli_tree.write(cli_output)
-        # Rewind to start of file for reading.
-        cli_tree.seek(0)
-        # Define variables for setting up list of dictionaries as tree.
-        indentation = []
-        indentation.append(0)
-        depth = 0
-        root = {"folder": "root", "id": "0", "depth": 0, "children": []}
-        parents = []
-        node = root
-        # 3) Parse file, replacing symbols with tabs.
-        with open(parsed_tree.name, "w+") as output_file:
-            for line in cli_tree:
-                # print(f"line: {line}")
-                match_first_parent = re.search(FIRST_PARENT_REGEX,line)
-                if match_first_parent:
-                    line = re.sub(FIRST_PARENT_REGEX, r'\1', line, re.MULTILINE)
-                match_last_parent = re.search(LAST_PARENT_REGEX, line)
-                if match_last_parent:
-                    line = re.sub(LAST_PARENT_REGEX, r'\1', line, re.MULTILINE)
-                    line = line.strip()
-                match_leaf = re.search(LEAF_REGEX, line)
-                if match_leaf:
-                    line = re.sub(LEAF_REGEX, r'\1\2', line, re.MULTILINE)
-                match_last_leaf = re.search(LAST_LEAF_REGEX, line)
-                if match_last_leaf:
-                    line = re.sub(LAST_LEAF_REGEX, r'\1\2', line, re.MULTILINE)
-                match_dangling_line = re.search(DANGLING_LINE_REGEX, line)
-                if match_dangling_line:
-                    line = re.sub(DANGLING_LINE_REGEX, r'\1\2', line, re.MULTILINE)
-                match = re.search(CLEAN_LINE_TO_YAML_REGEX,line)
-                if match:
-                    line = re.sub(CLEAN_LINE_TO_YAML_REGEX,r'\1\2:\3\n',line,re.MULTILINE)
-                else:
-                    line = re.sub(TOP_LEVEL_CLEAN_LINE_TO_YAML_REGEX,r'\1:\2\n',line,re.MULTILINE)
-                print(f"line: {line}")
-                output_file.write(line)
-            # Rewind temp file to beginning before parsing.
-            output_file.seek(0)
-            # 4) Parse text tree into list of dictionaries.
-            lines = [line.rstrip() for line in output_file.readlines()]
-            non_empty_lines = [line for line in lines if line != '']
-            for line in non_empty_lines:
-                # line = line[:-1]
-                content = line.lstrip()
-                currentIndentation = len(line) - len(content) + 1
-                folder_name, folder_id = content.split(':')
-
-                # If new line's indentation is shallower than previous, we need to remove items from the list
-                if currentIndentation < indentation[-1]:
-                    while currentIndentation < indentation[-1]:
-                        depth -= 1
-                        indentation.pop()
-                    if currentIndentation != indentation[-1]:
-                        raise RuntimeError("Bad formatting")
-                    parents = parents[:depth]
-                # If new line's indentation is deeper, we need to add the previous node
-                elif currentIndentation > indentation[-1]:
-                    depth += 1
-                    indentation.append(currentIndentation)
-                    parents.append(node)
-                # Create new node
-                node = {"folder": folder_name.strip(), "id": folder_id.strip(), "depth" : depth, "children": []}
-                parents[-1]['children'].append(node)
-        # 5) Close temporary files & return list of dictionaries.
-        print(root["children"])
-        parsed_tree.close()
-        cli_tree.close()
-        return(root["children"])
-
-
-def get_folder_id(name: str, tree: [] = None):
-    """
-    Given the name of a folder in the workpace and a list of dictionaries
-    representing the VWB workspace's folder tree, returns the ID corresponding to the folder name,
-    if it exists; else returns None.
+    Given a JSON representation of the folder tree,
+    returns a folder's ID if it exist, else None.
     """
     for item in tree:
-        current_name = item['folder'].strip()
+        current_name = item['name'].strip()
         if current_name == name:
             return item['id']
         folder_id_from_children = get_folder_id(name, item['children'])
@@ -117,30 +29,37 @@ def get_folder_id(name: str, tree: [] = None):
             return folder_id_from_children
     return None
 
-def get_folders_with_depth(workspace_id: str, depth: int):
+def get_folders_with_depth(depth: int, tree: []):
     """
     Given the workspace ID of a data collection, and a desired depth ( >= 1),
     returns a list of folders in the workspace with that depth.
     """
-    # 1) Get workspace folder tree, if not provided.
-    cli_folder_tree_output = subprocess.run(
-        ["terra", "folder", "tree", f"--workspace={workspace_id}"], capture_output=True, text=True
-    )
-    cli_output = cli_folder_tree_output.stdout
-    print(f"cli_output:\n{cli_output}")
-    
-    # 2) Parse tree to list of dictionaries.
-    tree = parse_tree(cli_output)
     folders_at_depth = []
     for item in tree:
         if item['depth'] == depth:
-            folders_at_depth.append(item['folder'].strip())
-    # 3) Return list of versions.
+            folders_at_depth.append(item['name'].strip())
+        elif item['children']:
+            folders_at_depth += get_folders_with_depth(depth, item['children'])
     return folders_at_depth
-
-def get_potential_versions(workspace_id: str):
+    
+def get_published_versions(tree: []):
     """
     Given a workspace ID, returns the top-level folders for that directory 
-    which are eligible for publication as a data collection version.
+    which are published versions of a data collection.
     """
-    return get_folders_with_depth(workspace_id, depth = 1)
+    versions = []
+    top_level_folders = get_folders_with_depth(1, tree)
+    print(f"top level: {top_level_folders}")
+    node = None
+    for folder_name in top_level_folders:
+        node = traverse_tree(folder_name, tree)
+        # If the top-level node has a 'properties' field, we check it for a `terra-published-date`.
+        if 'properties' in node:
+            properties = node['properties']
+            # Although this check seems redundant, it is not. This is necessary because users can set the properties field to the empty string to revert a data collection into its backing workspace via the CLI.
+            # This manifests in the JSON output as the properties field still existing but as an empty list of dictionaries.
+            if len(properties) !=0:
+                properties = properties[0]
+            if 'terra-published-date' in properties:
+                versions.append(node['name'])
+    return versions
